ba4ad2c19099f2c6502b4ee0bf84b9c3f866755d
Selaelo1 <selaelolanga10@gmail.com>
2024-10-25 14:40:52 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import favicon from 'vite-plugin-favicon';
+import { resolve } from 'path';
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    favicon({
+      logo: resolve(__dirname, 'public', 'favicon.png'),
+      inject: true,
+    }),
+  ],
 });
