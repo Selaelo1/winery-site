29b89218d674e88c31e9b4f9dd40f1dead999633
Selaelo1 <selaelolanga10@gmail.com>
2024-10-29 09:41:54 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,39 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Experiences from "./components/Experiences";
 import Wines from "./components/Wines";
 import Visit from "./components/Visit";
 import Footer from "./components/Footer";
+import TicketPage from "./components/TicketPage";
+import BookingConfirmation from "./components/BookingConfirmation";
 
 function App() {
   return (
-    <div className="min-h-screen">
-      <Navbar />
-      <Hero />
-      <Experiences />
-      <Wines />
-      <Visit />
-      <Footer />
-    </div>
+    <Router>
+      <div className="min-h-screen">
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Hero />
+                <Experiences />
+                <Wines />
+                <Visit />
+              </>
+            }
+          />
+          <Route path="/tickets" element={<TicketPage />} />
+          <Route
+            path="/booking-confirmation"
+            element={<BookingConfirmation />}
+          />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
