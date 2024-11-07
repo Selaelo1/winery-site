82f7bedfbdef284178f6b7be99d78bab578b4c3d
Selaelo1 <selaelolanga10@gmail.com>
2024-11-07 16:51:16 +0200

    Update Cart.tsx

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useState } from "react";
+import { useState } from "react";
 import { ShoppingCart, X, Truck, Store, Wallet } from "lucide-react";
 import DeliveryForm from "./DeliveryForm";
 
